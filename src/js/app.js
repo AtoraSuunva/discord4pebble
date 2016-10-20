216a319e1f9151755567b0924dd5bb4536c3438b
Googie2149 <gc2149@gmail.com>
2016-10-20 16:25:02 -0400

    Added message viewing support
    Messages are now cached, though I should move to storing message
    objects.

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,13 @@
+/*jshint -W041 */
+
 var version = '0.0.5';
 
 var UI = require('ui');
+var Voice = require('ui/voice');
+var Vibe = require('ui/vibe');
 var Discord = require('d4p.js');
 var Menufy = require('menufy.js');
-var token = '';
+var token = 'this needs to have a setting somewhere';
 
 var client = new Discord.Client({debug: true});
 var menufy = new Menufy.er();
@@ -12,6 +16,35 @@ var Settings = require('settings');
 var fontSize = Settings.option('fontSize'); //guess
 if (fontSize === undefined) fontSize = 0;
 var fontSizeName = ['small', 'large', 'mono', 'classic-small', 'classic-large'];
+
+var currentChannel = '0';
+var channelText = {};
+
+// Global channelCard so that it can be edited later
+var channelCard = new UI.Card({
+  scrollable: true
+});
+
+channelCard.on('longClick', function(e) { // Clear channel on long click
+  channelText[currentChannel] = [];
+  channelCard.body('');
+});
+
+channelCard.on('click', function(e) { // TODO: implement message sending so this actually does something
+  Voice.dictate('start', false, function(e) {
+    if (e.err) {
+      console.log('Error: ' + e.err);
+      return;
+    }
+
+    channelCard.subtitle('Success: ' + e.transcription);
+  });
+});
+
+channelCard.on('hide', function(q){
+  channelText[currentChannel] = channelText[currentChannel].slice(0, 3);
+  currentChannel = 0;
+});
 
 var loadingCard = new UI.Card({
   title: 'd4p',
@@ -26,13 +59,29 @@ client.on('ready', function(data) {
 	menuMain();
 });
 
-/*
+
 client.on('message', function(message) {
-	if (message.channel_id !== '147039491838705664') return;
-	card.title((message.member !== undefined && message.member.nick !== null) ? message.member.nick : message.author.username);
-	card.body ('#' + message.channel.name + '\n' + message.cleanContent);
+  if (channelText[message.channel_id] == null)
+    channelText[message.channel_id] = [];
+  
+  channelText[message.channel_id] = unshift(formatMessage(message), channelText[message.channel_id]);
+    // Insert a new message to the start of the array
+    // gotta love that array.unshift doesn't seem to be officially supported, maybe I just suck at JS
+  
+  if (channelText[message.channel_id].length > 6) // We're only cachine 6 messages currently
+    channelText[message.channel_id] = channelText[message.channel_id].slice(0, 6);
+  
+	if (message.channel_id !== currentChannel) return; // If we're not viewing the channel, we don't need to do anything
+  
+  channelCard.title('#' + message.channel.name);
+  channelCard.body(displayMessages(channelText[message.channel_id]));
+  channelCard.style(fontSizeName[fontSize]);
+  
+  message.mentions.forEach(function(user) { // Vibrate if mentioned
+    if (user.id == client.user[0].id)
+      Vibe.vibrate('double');
+  });
 });
-*/
 
 function menuMain() {
 	var menu = new UI.Menu({
@@ -105,17 +154,26 @@ function menuChannels(guildId) {
 	
 	//console.log(check(curGuild.members));
 	
-	menu.on('select', function(e) {
-		//menuChannels(e.item.id);
-	});
-
 	menu.on('longSelect', function(e) {
-		var channelCard = new UI.Card({
+		var channelDetails = new UI.Card({
 			title: '#' + e.item.name,
 			body: e.item.topic,
 			scrollable: true,
 			style: fontSizeName[fontSize]
 		});
+    
+    channelDetails.show();
+	});
+
+	menu.on('select', function(e) {
+    if (channelText[e.item.id] == null) // Check if we have any messages cached
+      channelText[e.item.id] = []; // if no, create a new array to prevent chaos
+    
+    channelCard.title('#' + e.item.name);
+    channelCard.body(displayMessages(channelText[e.item.id]));
+    channelCard.style(fontSizeName[fontSize]);
+    currentChannel = e.item.id;
+    
 		channelCard.show();
 	});
 	
@@ -161,6 +219,39 @@ client.login(token);
 // Helper Functions
 function capitalize(str) { //I'm lazy!
 	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function unshift(item, array) {
+  var newArray = new Array(array.length + 1);
+  newArray[0] = item;
+  for (var i = 1; i < newArray.length; i++) {
+    newArray[i] = array[i - 1];
+  }
+  
+  return newArray;
+}
+
+function displayMessages(array) {
+  var text = '';
+  
+  array.forEach(function(message) {
+    if (text === '')
+      text = message;
+    else
+      text = text + '\n' + message;
+  });
+  
+  return text;
+}
+
+function formatMessage(message) { // Takes a message object and turns mentions into readable text, and checks nicknames
+  // A lot of this is assuming that we're in a server and not a private message.
+  
+  var content = message.cleanContent;
+  if (content.length > 250) // Trim the message to 250 characters to keep from crashing, though this may not help much
+    content = content.substring(0, 250) + '...'; // TODO: find the closest space so it isn't cut in the middle of a word.
+  var text = '@' + message.author.username + ': ' + content;
+  return text;
 }
 
 function convertTimestamp(timestamp) {
