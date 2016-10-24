fef02d0cb1454cb238f60f36ca6670b8986b00e2
Atora <at@atora.dev>
2016-10-23 23:03:53 -0400

    Messages now actually work
    This version uses menus instead of a card like master does

--- src/js/menufy.js
+++ src/js/menufy.js
@@ -32,8 +32,6 @@ module.exports.er = function(opt) {
 	this.channels = function(channels) { //no order param since the channels have a 'position' property
 		channels.sort(function(a,b) {return (a.position < b.position) ? 1 : ((b.position < a.position) ? -1 : 0);} );
 		
-		console.log(JSON.stringify(channels, null, 2));
-		
 		var items = [];
 		channels.forEach(function(channel) {
 			if (channel.type !== 'text') return;
@@ -48,4 +46,16 @@ module.exports.er = function(opt) {
 		return items;
 	};
 	
+	this.messages = function(messages) {
+		var items = [];
+		
+		messages.forEach(function(message) {
+			items.push({
+				title: message.author.username,
+				subtitle: message.cleanContent,
+				message: message
+			});
+		});
+		return items;
+	};
 };
